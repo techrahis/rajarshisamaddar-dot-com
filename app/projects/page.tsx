c178edb1cca627c94a45010840a58fb5ac9eedbd
rajarshisamaddar <itsrajarshi@gmail.com>
2023-08-22 22:28:08 +0530

    feat: projects update

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,8 +13,9 @@ export default function Page() {
         <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
           Projects
         </h1>
-        <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-          Here are some of my selected projects worth sharing.
+        <p className="text-xl leading-7 text-gray-500 dark:text-gray-400">
+          I've been making various types of projects some of them were basics and some of them were
+          complicated. So far I've made <b>14+</b> projects.
         </p>
       </div>
       <Projects />
--- components/Projects/ProjectItem.tsx
+++ components/Projects/ProjectItem.tsx
@@ -10,24 +10,34 @@ interface ProjectProps {
 
 export default function ProjectItem({ index, title, url, role, setModal }: ProjectProps) {
   return (
-    <a
-      href={url}
-      target="_blank"
-      onMouseEnter={() => {
-        setModal({ active: true, index });
-      }}
-      onMouseLeave={() => {
-        setModal({ active: false, index });
-      }}
-      className="group flex w-full items-center justify-between border-b px-4 py-10 sm:px-10 sm:py-16"
-      rel="noreferrer"
-    >
-      <h2 className="text-2xl transition-all group-hover:-translate-x-3 group-hover:scale-110 sm:text-6xl">
-        {title}
-      </h2>
-      <p className="text-sm font-light transition-all group-hover:translate-x-3 group-hover:scale-110 sm:text-lg">
-        {role}
-      </p>
-    </a>
+    <>
+      <a
+        href={url}
+        target="_blank"
+        onMouseEnter={() => {
+          setModal({ active: true, index });
+        }}
+        onMouseLeave={() => {
+          setModal({ active: false, index });
+        }}
+        className="border-b"
+        rel="noreferrer"
+      >
+        <div className="group flex w-full items-center justify-between pt-8">
+          <h2 className="text-2xl transition-all group-hover:-translate-x-3 group-hover:scale-110 sm:text-6xl">
+            {title}
+          </h2>
+          <p className="text-sm font-light transition-all group-hover:translate-x-3 group-hover:scale-110 sm:text-lg">
+            {role}
+          </p>
+        </div>
+
+        <div className="flex flex-wrap items-center gap-1 border-b pt-4 pb-8">
+          <span className="rounded border border-gray-200 px-2 py-1 text-xs text-gray-500 shadow-md dark:border-gray-800 dark:bg-zinc-800 dark:text-gray-100">
+            Next.js
+          </span>
+        </div>
+      </a>
+    </>
   );
 }
