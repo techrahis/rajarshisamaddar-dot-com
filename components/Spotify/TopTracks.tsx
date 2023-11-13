29a5f3a9fda1d9a9f238c44164fa4117fae5a66a
Rajarshi Samaddar <itsrajarshi@hotmail.com>
2023-11-13 19:39:21 +0530

    fix: spotify error ✅

--- components/Spotify/TopTracks.tsx
+++ components/Spotify/TopTracks.tsx
@@ -10,19 +10,22 @@ export default function TopTracks() {
   );
 
   if (topTracks) {
-    return (
-      <div className="pt-10">
-        <h1 className="mb-2 text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-10 md:text-4xl md:leading-14">
-          My <span className="text-green-700 dark:text-green-500">Spotify</span> Top Streamed Songs
-        </h1>
-        <p className="mb-10 font-medium">
-          <span className="font-semibold">{topTracks[0].title}</span> is the most streamed song of
-          mine in last 4 weeks. Here's my top tracks on Spotify updated daily.
-        </p>
-        {topTracks.map((track: any, index: any) => (
-          <Track ranking={index + 1} key={track.songUrl} track={track} />
-        ))}
-      </div>
-    );
+    if (topTracks.length > 0) {
+      return (
+        <div className="pt-10">
+          <h1 className="mb-2 text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-10 md:text-4xl md:leading-14">
+            My <span className="text-green-700 dark:text-green-500">Spotify</span> Top Streamed
+            Songs
+          </h1>
+          <p className="mb-10 font-medium">
+            <span className="font-semibold">{topTracks[0].title}</span> is the most streamed song of
+            mine in last 4 weeks. Here's my top tracks on Spotify updated daily.
+          </p>
+          {topTracks.map((track: any, index: any) => (
+            <Track ranking={index + 1} key={track.songUrl} track={track} />
+          ))}
+        </div>
+      );
+    }
   }
 }
